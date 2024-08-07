2613bb231a38deaada48f1128527803b7ac7d6f4
Pierre-Paul TACHER <pierre-paul@Pierre-PaulnoMacBook-Pro.local>
2024-08-07 23:33:52 +0200

    all routes implemented.

--- routes/middleware/cors.js
+++ routes/middleware/cors.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + process.env.PORT_FE);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
